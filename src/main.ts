3dc2f5ff38b7e23af46cbc4466a753bce21349cf
k-okina <okinakahiro@gmail.com>
2019-11-07 16:06:38 +0900

    わざとミスってみる

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ const person = {
   firstName: '太郎',
   lastName: '田中',
   getFullName() {
+    setInterval(function() {
+      console.log(`${this.lastName} ${this.firstName}`)
+    })
     return `${this.lastName} ${this.firstName}`
   }
 }
